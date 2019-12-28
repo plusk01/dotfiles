796d92e5106e8122459f5f6715e2c7d9965785b4
Parker Lusk <parkerclusk@gmail.com>
2019-12-27 20:46:46 -0500

    vim plugins and tmux completion

--- install.sh
+++ install.sh
@@ -7,3 +7,12 @@ cp luskrc ~/.luskrc
 
 # TODO: make sure this line doesn't already exist
 echo "source ~/.luskrc" >> ~/.bashrc
+
+# Install vim plugins
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall # make sure that your .vimrc is populated with plugins
+
+# Install YouCompleteMe
+#sudo apt install -y build-essential cmake
+#sudo apt install -y python-dev python3-dev
+cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer
