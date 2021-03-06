f2faf19eeea63728586d3c527feae3c7090641d1
Parker Lusk <parkerclusk@gmail.com>
2021-03-06 11:58:25 -0500

    lsg

--- install.sh
+++ install.sh
@@ -4,8 +4,12 @@
 MYPATH=$(dirname ${BASH_SOURCE[0]})
 ORIGIN=$(dirname $(readlink -f $0))
 
-cp luskrc $HOME/.luskrc
-sed -i s~__DOTFILES__~$ORIGIN~g $HOME/.luskrc
+if [ -f "$HOME/.luskrc" ]; then
+  echo "$HOME/.luskrc already exists -- not copying"
+else
+  cp luskrc $HOME/.luskrc
+  sed -i s~__DOTFILES__~$ORIGIN~g $HOME/.luskrc
+fi
 
 # Make sure this line doesn't already exist
 if ! grep -Fq ".luskrc" $HOME/.bashrc; then
@@ -21,6 +25,11 @@ cp tmux.conf $HOME/.tmux.conf
 
 # copy vimrc
 cp vimrc $HOME/.vimrc
+
+# Install lsg tool
+git clone https://github.com/gerph/ls-with-git-status /tmp/lsg
+mv /tmp/lsg/lsg $HOME/.local/bin/
+rm -rf /tmp/lsg
 
 # Install vim plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
